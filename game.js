4efa23542168fcef98ac23fd63b3f74e7de1d3dd
judith pizzolato <judith pizzolato>
2016-07-18 14:59:38 -0400

    animation of stickman

---
+++ game.js
@@ -0,0 +1,92 @@
+// Animate man
+    var animate = function () {
+        var drawMe = lives;
+        drawArray[drawMe]();
+    }
+
+    // Hangman
+    canvas = function () {
+        myStickman = document.getElementById('stickman');
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.strokeStyle = '#fff';
+        context.lineWidth = 2;
+    };
+
+    head = function () {
+        myStickman = document.getElementById('stickman');
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.arc(60, 25, 10, 0, Math.PI * 2, true);
+        context.stroke();
+    }
+    draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
+        context.moveTo($pathFromx, $pathFromy);
+        context.lineTo($pathTox, $pathToy);
+        context.stroke();
+    }
+
+    frame1 = function () {
+        draw(0, 150, 150, 150);
+    };
+    frame2 = function () {
+        draw(10, 0, 10, 600);
+    };
+    frame3 = function () {
+        draw(0, 5, 70, 5);
+    };
+    frame4 = function () {
+        draw(60, 5, 60, 15);
+    };
+    torso = function () {
+        draw(60, 36, 60, 70);
+    };
+    rightArm = function () {
+        draw(60, 46, 100, 50);
+    };
+    leftArm = function () {
+        draw(60, 46, 20, 50);
+    };
+    rightLeg = function () {
+        draw(60, 70, 100, 100);
+    };
+    leftLeg = function () {
+        draw(60, 70, 20, 100);
+    };
+    drawArray = [
+        rightLeg,
+        leftLeg,
+        rightArm,
+        leftArm,
+        torso,
+        head,
+        frame4,
+        frame3,
+        frame2,
+        frame1
+    ];
+
+    // OnClick Function
+    check = function () {
+        list.onclick = function () {
+            var guess = this.innerHTML;
+            this.setAttribute('class', 'active');
+            this.onclick = null;
+            for (var i = 0; i < word.length; i++) {
+
+                if (word[i] === guess) {
+                    guesses[i].innerHTML = guess;
+                    counter += 1;
+                }
+            }
+
+            var j = (word.indexOf(guess));
+            if (j === -1) {
+                lives -= 1;
+                comments();
+                animate();
+            } else {
+                comments();
+            }
+        }
+    }
